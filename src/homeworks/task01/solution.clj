df807851c1bd9a09bd1ca16b86e1a1778c7d69ff
Dimo Hubenov <dimexer@debian.workgroup>
2013-03-09 18:30:36 +0200

    added make-bisector function

--- src/homeworks/task01/solution.clj
+++ src/homeworks/task01/solution.clj
@@ -9,3 +9,15 @@
             (if (> (f c) 0)
               (recur f neg-point c close-enough?)
               (recur f c pos-point close-enough?)))))))
+
+(defn make-bisector
+  [tolerance]
+  (fn[f a b]
+    (defn tol[a b]
+      (if (< a b) (< (- b a) tolerance) (< (- a b) tolerance)))
+    (if (neg? (f a))
+;      (if (neg? (f b)) nil (bisect f a b (fn [a b](< (- a b) tolerance))))
+;      (if (pos? (f b)) nil (bisect f b a (fn [a b](< (- a b) tolerance)))))))
+      (if (neg? (f b)) nil (bisect f a b tol))
+      (if (pos? (f b)) nil (bisect f a b tol)))))
+
