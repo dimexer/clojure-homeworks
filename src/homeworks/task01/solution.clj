e6f08f3c84c43143e9cb2eb875edfd76c46c1e09
Dimo Hubenov <dimexer@debian.workgroup>
2013-03-13 20:56:27 +0200

    code formatted

--- src/homeworks/task01/solution.clj
+++ src/homeworks/task01/solution.clj
@@ -1,20 +1,15 @@
-(defn bisect
-  [f neg-point pos-point close-enough?]
-  (let [c (/ (+ neg-point pos-point) 2)]
-    (if (close-enough? neg-point pos-point)
-      c
-      (do
-          (if (zero? (f c))
-            c
+(defn bisect [f negp posp close-enough?]
+  (let [c (/ (+ negp posp) 2)]
+    (if (close-enough? negp posp) c
+      (do (if (zero? (f c)) c
             (if (pos? (f c))
-              (recur f neg-point c close-enough?)
-              (recur f c pos-point close-enough?)))))))
+              (recur f negp c close-enough?)
+              (recur f c posp close-enough?)))))))
 
-(defn make-bisector
-  [tolerance]
+(defn make-bisector [tol]
   (fn[f a b]
     (letfn [(eq? [a b]
-              (< (if (< a b) (- b a) (- a b)) tolerance))]
+              (< (if (< a b) (- b a) (- a b)) tol))]
       (if (neg? (f a))
         (if (neg? (f b)) nil (bisect f a b eq?))
         (if (pos? (f b)) nil (bisect f b a eq?))))))
@@ -25,6 +20,6 @@
 
 (defn peek-at-queue [q] (first q))
 
-(defn pop-from-queue [q](vec (subvec q 1)))
+(defn pop-from-queue [q] (vec (subvec q 1)))
 
 (defn empty-queue? [q] (empty? q))
